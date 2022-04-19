bd324e9f2bfa84451ba730a03589d94581985edd
Pierre Venet <XB32Z@users.noreply.github.com>
2022-04-19 15:39:02 +0200

    Use HashMap, fix typos and use conventions

--- src/marker.rs
+++ src/marker.rs
@@ -2,11 +2,11 @@
 //!
 //! ROS has a type of message dedicated to visualization: visualization_msgs::Marker.
 //! This module allows to subsribe to topics that publish them and project them into the
-//! 2D offset.y + dimension.y / 2.0ane. Finally, it takes care of their lifecycle: ADD, DELETE and timeout.
+//! 2D plane. Finally, it takes care of their lifecycle: ADD, DELETE and timeout.
 use crate::config::ListenerConfig;
 use nalgebra::base::Vector3;
 use nalgebra::geometry::Isometry3;
-use std::collections::BTreeMap;
+use std::collections::HashMap;
 use std::f64::consts::PI;
 use std::sync::{Arc, Mutex, RwLock};
 
@@ -22,8 +22,8 @@ struct TermvizMarker {
     pub id: i32,
 }
 
-///Creates a list of lines from N line stips.
-/// Arguments:
+/// Creates a list of lines from N line strips.
+/// # Arguments
 /// - `strips`: A vector of vector of points. Each element is a strip, i.e. a single
 ///             broken line. Each strip has N points that form N-1 lines.
 fn from_point_strips(strips: &Vec<Vec<Point3<f64>>>, color: &Color) -> Vec<Line> {
@@ -49,11 +49,11 @@ fn from_point_strips(strips: &Vec<Vec<Point3<f64>>>, color: &Color) -> Vec<Line>
     lines
 }
 
-///Creates the visible lines for a cube.
+/// Creates the visible lines for a cube.
 ///
-/// If the cube is parallel to the z-offset.y + dimension.y / 2.0an, the visible lines are simoffset.y + dimension.y / 2.0y the 4 top ones.
+/// If the cube is parallel to the plan, the visible lines are simoffset.y + dimension.y / 2.0y the 4 top ones.
 /// Else, we draw all 12 edges.
-/// Arguments:
+/// # Arguments:
 /// - `dimension`: size of the cube as width, length, height.
 /// - `offset`: Offset of the center of the cube in the iso transformation.
 /// - `color`: Color of the cube.
@@ -66,7 +66,7 @@ fn parse_cube(
 ) -> Vec<Line> {
     let angles = iso.rotation.euler_angles();
 
-    let mut pointss: Vec<Vec<Point3<f64>>> = Vec::new();
+    let mut points_strips: Vec<Vec<Point3<f64>>> = Vec::new();
 
     let face_top = vec![
         iso.transform_point(&Point3::new(
@@ -95,7 +95,7 @@ fn parse_cube(
             offset.z + dimension.z / 2.0,
         )),
     ];
-    pointss.push(face_top);
+    points_strips.push(face_top);
 
     if angles.0.abs() > 0.0001 || angles.1.abs() > 0.0001 {
         // Other faces may be visible, render all of them
@@ -174,10 +174,10 @@ fn parse_cube(
                 offset.z - dimension.z / 2.0,
             )),
         ];
-        pointss.extend([face_bottom, a, b, c, d]);
+        points_strips.extend([face_bottom, a, b, c, d]);
     }
 
-    return from_point_strips(&pointss, color);
+    return from_point_strips(&points_strips, color);
 }
 
 fn parse_arrow_msg(
@@ -385,16 +385,16 @@ fn parse_marker_msg(
     }
 }
 
-///Class that holds all the markers currently active.
+/// Class that holds all the markers currently active.
 ///
 /// The markers are ordered in a double dictionary, which allows to manage namespaces.
 /// Each namespace has its own set of IDs. This structure allows to have markers with
 /// same IDs on different namespaces. Note that this class should be shared by all
 /// publishers such that they can be managed globally.
 struct TermvizMarkerContainer {
-    _markers: BTreeMap<String, BTreeMap<i32, TermvizMarker>>,
-    _static_frame: String,
-    _tf_listener: Arc<rustros_tf::TfListener>,
+    markers: HashMap<String, HashMap<i32, TermvizMarker>>,
+    static_frame: String,
+    tf_listener: Arc<rustros_tf::TfListener>,
 }
 
 impl TermvizMarkerContainer {
@@ -403,16 +403,16 @@ impl TermvizMarkerContainer {
         static_frame: String,
     ) -> TermvizMarkerContainer {
         Self {
-            _markers: BTreeMap::<String, BTreeMap<i32, TermvizMarker>>::new(),
-            _static_frame: static_frame,
-            _tf_listener: tf_listener,
+            markers: HashMap::<String, HashMap<i32, TermvizMarker>>::new(),
+            static_frame: static_frame,
+            tf_listener: tf_listener,
         }
     }
 
     fn add_marker(&mut self, marker: &rosrust_msg::visualization_msgs::Marker) {
-        let transform = &self._tf_listener.clone().lookup_transform(
+        let transform = &self.tf_listener.clone().lookup_transform(
             &marker.header.frame_id,
-            &self._static_frame.clone(),
+            &self.static_frame.clone(),
             marker.header.stamp,
         );
         match &transform {
@@ -420,7 +420,7 @@ impl TermvizMarkerContainer {
             Err(_e) => return,
         };
 
-        self._markers
+        self.markers
             .entry(marker.ns.clone())
             .and_modify(|namespace| {
                 let res = parse_marker_msg(&marker, &transform.as_ref().unwrap().transform);
@@ -428,25 +428,25 @@ impl TermvizMarkerContainer {
             })
             .or_insert_with(|| {
                 let res = parse_marker_msg(&marker, &transform.as_ref().unwrap().transform);
-                let mut namespace = BTreeMap::<i32, TermvizMarker>::new();
+                let mut namespace = HashMap::<i32, TermvizMarker>::new();
                 namespace.insert(res.id, res);
                 namespace
             });
     }
 
     fn delete_marker(&mut self, marker_ns: String, marker_id: i32) {
-        self._markers.entry(marker_ns).and_modify(|namespace| {
+        self.markers.entry(marker_ns).and_modify(|namespace| {
             namespace.remove(&marker_id);
         });
     }
 
     fn clear(&mut self) {
-        self._markers.clear();
+        self.markers.clear();
     }
 
     fn clear_namespace(&mut self, marker_ns: String) -> Vec<i32> {
         let mut res = Vec::new();
-        self._markers.entry(marker_ns).and_modify(|namespace| {
+        self.markers.entry(marker_ns).and_modify(|namespace| {
             for marker in namespace.values() {
                 res.push(marker.id);
             }
@@ -457,7 +457,7 @@ impl TermvizMarkerContainer {
 
     fn get_lines(&self) -> Vec<Line> {
         let mut res = Vec::<Line>::new();
-        for namespace in self._markers.values() {
+        for namespace in self.markers.values() {
             for marker in namespace.values() {
                 res.extend(marker.lines.to_vec());
             }
@@ -466,7 +466,7 @@ impl TermvizMarkerContainer {
     }
 }
 
-///Class that handles the lifecycle of the markers.
+/// Class that handles the lifecycle of the markers.
 ///
 /// Markers that provide a lifecycle (i.e. not 0) need to be deleted from the container
 /// once the lifecycle is reached. To do this we add another container which has a timer.
@@ -476,23 +476,24 @@ impl TermvizMarkerContainer {
 /// and a list. A repeated task is given to the timer to consume the list of deleted
 /// marker and free the guards.
 struct MarkersLifecycle {
-    _markers_container: Arc<RwLock<TermvizMarkerContainer>>,
-    _deleted_markers: Arc<Mutex<Vec<(String, i32)>>>,
-    _guards: Arc<Mutex<BTreeMap<(String, i32), timer::Guard>>>,
-    _timer: Arc<Mutex<timer::Timer>>,
-    _cleaner_guard: timer::Guard,
+    markers_container: Arc<RwLock<TermvizMarkerContainer>>,
+    deleted_markers: Arc<Mutex<Vec<(String, i32)>>>,
+    guards: Arc<Mutex<HashMap<(String, i32), timer::Guard>>>,
+    timer: Arc<Mutex<timer::Timer>>,
+    #[allow(dead_code)]  // because the guard is never used but must be kept
+    cleaner_guard: timer::Guard,
 }
 
 impl MarkersLifecycle {
     pub fn new(marker_container: TermvizMarkerContainer) -> MarkersLifecycle {
         let timer = Arc::new(Mutex::new(timer::Timer::new()));
         let deleted_markers = Arc::new(Mutex::new(Vec::<(String, i32)>::new()));
-        let guards = Arc::new(Mutex::new(BTreeMap::<(String, i32), timer::Guard>::new()));
+        let guards = Arc::new(Mutex::new(HashMap::<(String, i32), timer::Guard>::new()));
 
         let clean_job_delete_markers_ref = deleted_markers.clone();
         let clean_job_guards_ref = guards.clone();
 
-        let cleaner_guard =
+        let guard =
             timer
                 .lock()
                 .unwrap()
@@ -507,23 +508,23 @@ impl MarkersLifecycle {
                 });
 
         Self {
-            _markers_container: Arc::new(RwLock::new(marker_container)),
-            _deleted_markers: deleted_markers,
-            _guards: guards,
-            _timer: timer,
-            _cleaner_guard: cleaner_guard,
+            markers_container: Arc::new(RwLock::new(marker_container)),
+            deleted_markers: deleted_markers,
+            guards: guards,
+            timer: timer,
+            cleaner_guard: guard,
         }
     }
 
     fn add_marker(&mut self, marker: &rosrust_msg::visualization_msgs::Marker) {
-        self._markers_container.write().unwrap().add_marker(marker);
+        self.markers_container.write().unwrap().add_marker(marker);
 
         // Handle marker lifecycle
         if marker.lifetime.seconds() == 0.0 {
             return;
         }
 
-        let markers_container_ref = self._markers_container.clone();
+        let markers_container_ref = self.markers_container.clone();
 
         let chrono_delay = chrono::Duration::seconds(marker.lifetime.sec as i64)
             + chrono::Duration::nanoseconds(marker.lifetime.nsec as i64);
@@ -531,7 +532,7 @@ impl MarkersLifecycle {
         let marker_info = (marker.ns.clone(), marker.id);
 
         let guard_ = self
-            ._timer
+            .timer
             .lock()
             .unwrap()
             .schedule_with_delay(chrono_delay, move || {
@@ -539,16 +540,16 @@ impl MarkersLifecycle {
                 mc.delete_marker(marker_info.0.clone(), marker_info.1);
             });
 
-        let mut guards_ref = self._guards.lock().unwrap();
+        let mut guards_ref = self.guards.lock().unwrap();
         guards_ref.insert((marker.ns.clone(), marker.id), guard_);
     }
 
     fn delete_marker(&mut self, marker_ns: String, marker_id: i32) {
-        self._markers_container
+        self.markers_container
             .write()
             .unwrap()
             .delete_marker(marker_ns.clone(), marker_id);
-        self._deleted_markers
+        self.deleted_markers
             .lock()
             .unwrap()
             .push((marker_ns, marker_id));
@@ -556,57 +557,55 @@ impl MarkersLifecycle {
 
     fn clear(&mut self) {
         // loose the guards if any, to cancel the timer tasks
-        self._deleted_markers.lock().unwrap().clear();
-        self._guards.lock().unwrap().clear();
+        self.deleted_markers.lock().unwrap().clear();
+        self.guards.lock().unwrap().clear();
 
-        self._markers_container.write().unwrap().clear();
+        self.markers_container.write().unwrap().clear();
     }
 
     fn clear_namespace(&mut self, marker_ns: String) {
         let removed_ids = self
-            ._markers_container
+            .markers_container
             .write()
             .unwrap()
             .clear_namespace(marker_ns.clone());
 
         // schedule deletion of the cleared markers
-        let mut deleted_markers = self._deleted_markers.lock().unwrap();
-        for id in removed_ids {
-            deleted_markers.push((marker_ns.clone(), id));
-        }
+        let mut deleted_markers = self.deleted_markers.lock().unwrap();
+        deleted_markers.extend(removed_ids.iter().map(|&id| (marker_ns.clone(), id)));
     }
 
     fn get_lines(&self) -> Vec<Line> {
-        self._markers_container.write().unwrap().get_lines()
+        self.markers_container.write().unwrap().get_lines()
     }
 }
 
 pub struct MarkersListener {
-    _markers_lifecycle: Arc<RwLock<MarkersLifecycle>>,
-    _subscribers: Vec<Arc<Mutex<rosrust::Subscriber>>>,
+    markers_lifecycle: Arc<RwLock<MarkersLifecycle>>,
+    subscribers: Vec<Arc<Mutex<rosrust::Subscriber>>>,
 }
 
 impl MarkersListener {
     pub fn new(tf_listener: Arc<rustros_tf::TfListener>, static_frame: String) -> MarkersListener {
         let marker_container = TermvizMarkerContainer::new(tf_listener, static_frame);
         Self {
-            _markers_lifecycle: Arc::new(RwLock::new(MarkersLifecycle::new(marker_container))),
-            _subscribers: Vec::new(),
+            markers_lifecycle: Arc::new(RwLock::new(MarkersLifecycle::new(marker_container))),
+            subscribers: Vec::new(),
         }
     }
 
     /// Gets all the lines currently active, to render.
     pub fn get_lines(&self) -> Vec<Line> {
-        let markers_container_ref = self._markers_lifecycle.read().unwrap();
+        let markers_container_ref = self.markers_lifecycle.read().unwrap();
         markers_container_ref.get_lines()
     }
 
     /// Adds a subscriber for a marker topic.
     ///
-    /// Arguments:
+    /// # Arguments
     /// - `config`: Configuration containing the topic name.
     pub fn add_marker_listener(&mut self, config: &ListenerConfig) {
-        let markers_container_ref = self._markers_lifecycle.clone();
+        let markers_container_ref = self.markers_lifecycle.clone();
 
         let sub = rosrust::subscribe(
             &config.topic,
@@ -627,7 +626,7 @@ impl MarkersListener {
             },
         );
 
-        self._subscribers.push(Arc::new(Mutex::new(sub.unwrap())));
+        self.subscribers.push(Arc::new(Mutex::new(sub.unwrap())));
     }
 
     /// Adds a subscriber for a marker array message topic.
@@ -635,7 +634,7 @@ impl MarkersListener {
     /// # Arguments
     /// * `config` - Configuration containing the topic.
     pub fn add_marker_array_listener(&mut self, config: &ListenerConfig) {
-        let markers_container_ref = self._markers_lifecycle.clone();
+        let markers_container_ref = self.markers_lifecycle.clone();
 
         let sub = rosrust::subscribe(
             &config.topic,
@@ -660,6 +659,6 @@ impl MarkersListener {
             },
         );
 
-        self._subscribers.push(Arc::new(Mutex::new(sub.unwrap())));
+        self.subscribers.push(Arc::new(Mutex::new(sub.unwrap())));
     }
 }
